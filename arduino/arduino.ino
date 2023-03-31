b5ff6a8c6865dfae9180972876294da9c3646562
shadow578 <52449218+shadow578@users.noreply.github.com>
2023-03-31 21:12:58 +0200

    update arduino firmware

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,139 +1,216 @@
 //
-// Basic Configuration
+// Configuration
 //
 #define COMM_BAUD_RATE 115200
+#define OWN_DEVICE_ID 0xCA
 
-#define MIN_PIN_NUMBER 2
-#define MAX_PIN_NUMBER 13
+#define IS_VALID_DIGITAL_PIN(x) ((x) >= 2 && (x) <= 13)
+#define IS_VALID_ANALOG_PIN(x) ((x) >= 0 && (x) <= 8)
+
+#define PIN_NO_TO_DIGITAL_PIN(x) (x)
+#define PIN_NO_TO_ANALOG_PIN(x) (A0 + x)
 
 //
-// Macros
+// SDSP
 //
-#define IS_VALID_PIN(x) ((x) >= MIN_PIN_NUMBER && (x) <= MAX_PIN_NUMBER)
-
-//
-// Protocol Constants
-//
-#define PKG_START_BYTE '{'
-#define PKG_END_BYTE '}'
-#define PKG_CHKSUM_PLACEHOLDER_VALUE 0x00
-#define PKG_RESPONSE_ERROR_MASK 0x80
-
-#define CMD_DIGITAL_READ 0x1
-#define CMD_DIGITAL_READ_PULLUP 0x2
-#define CMD_DIGITAL_WRITE 0x3
-#define CMD_ANALOG_READ 0x4
-#define CMD_ANALOG_WRITE 0x5
-
-#define ERR_MALFORMED_PACKET 0x1
-#define ERR_INVALID_CHECKSUM 0x2
-#define ERR_INVALID_PIN 0x3
-#define ERR_INVALID_COMMAND 0x4
-
-void sendResponse(uint8_t cmd, uint8_t result, bool success)
+#include "sdsp.h"
+uint8_t sdsp_serial_read_blocking()
 {
-    // set error bit
-    cmd = (cmd & ~PKG_RESPONSE_ERROR_MASK) | (success ? 0 : PKG_RESPONSE_ERROR_MASK);
-
-    // send package
-    uint8_t chksum = PKG_START_BYTE + cmd + result + PKG_CHKSUM_PLACEHOLDER_VALUE + PKG_END_BYTE;
-    Serial.write(PKG_START_BYTE);
-    Serial.write(cmd);
-    Serial.write(result);
-    Serial.write(chksum);
-    Serial.write(PKG_END_BYTE);
-    Serial.flush();
-}
-
-void executeCommand(uint8_t cmd, uint8_t pin, uint8_t val)
-{
-    // validate pin number
-    if (!IS_VALID_PIN(pin))
-    {
-        sendResponse(cmd, ERR_INVALID_PIN, false);
-        return;
-    }
-
-    // execute command
-    switch (cmd)
-    {
-    case CMD_DIGITAL_READ:
-    case CMD_DIGITAL_READ_PULLUP:
-        pinMode(pin, cmd == CMD_DIGITAL_READ_PULLUP ? INPUT_PULLUP : INPUT);
-        sendResponse(cmd, digitalRead(pin) ? 1 : 0, true);
-        break;
-    case CMD_DIGITAL_WRITE:
-        pinMode(pin, OUTPUT);
-        digitalWrite(pin, val);
-        sendResponse(cmd, val, true);
-        break;
-    case CMD_ANALOG_READ:
-        pinMode(pin, INPUT);
-        sendResponse(cmd, analogRead(pin) & 0xFF, true);
-        break;
-    case CMD_ANALOG_WRITE:
-        pinMode(pin, OUTPUT);
-        analogWrite(pin, val);
-        sendResponse(cmd, val, true);
-        break;
-    default:
-        // invalid command, ignore it
-        sendResponse(cmd, ERR_INVALID_COMMAND, false);
-        break;
-    }
-}
-
-void acceptPackage()
-{
-    // check for start of packet
-    uint8_t start = Serial.read();
-    if (start != PKG_START_BYTE)
-    {
-        return;
-    }
-
-    // wait for the remaining data
-    while (Serial.available() < 5)
+    while (Serial.available() == 0)
         ;
+    return Serial.read();
+}
 
-    // read the packet data
-    uint8_t cmd = Serial.read();
-    uint8_t pin = Serial.read();
-    uint8_t val = Serial.read();
+void sdsp_serial_write(uint8_t data)
+{
+    Serial.write(data);
+}
 
-    // read package epilogue
-    uint8_t chksum = Serial.read();
-    uint8_t end = Serial.read();
-    if (end != PKG_END_BYTE)
+//
+// Command protocol constants
+//
+#define TYPE_READ_REQUEST 0x01
+#define TYPE_WRITE_REQUEST 0x02
+#define TYPE_READ_RESPONSE 0x03
+#define TYPE_WRITE_RESPONSE 0x04
+#define TYPE_ERROR_RESPONSE 0x05
+
+union read_requests_flags
+{
+    uint8_t flags;
+    struct
     {
-        // invalid package, ignore it
-        sendResponse(cmd, ERR_MALFORMED_PACKET, false);
+        uint8_t pullup : 1;
+        uint8_t pulldown : 1;
+        uint8_t analog : 1;
+        uint8_t reserved : 5;
+    };
+};
+
+union write_requests_flags
+{
+    uint8_t flags;
+    struct
+    {
+        uint8_t analog : 1;
+        uint8_t reserved : 7;
+    };
+};
+
+#define ERR_MALFORMED_PACKET 0x01
+#define ERR_INVALID_TYPE 0x02
+#define ERR_INVALID_PIN 0x03
+
+//
+// Implementation
+//
+#define PKG_BUFFER_LEN 32
+uint8_t pkg_buffer[PKG_BUFFER_LEN];
+
+void send_error_response(uint8_t error_code, uint8_t to)
+{
+    uint8_t pkg[2] = {TYPE_ERROR_RESPONSE, error_code};
+    sdsp_write_packet(pkg, 2, OWN_DEVICE_ID, to);
+}
+
+void handle_packet(uint8_t pkg_buffer[], uint16_t pkg_len, uint8_t from)
+{
+    // read packet type
+    if (pkg_len < 1)
+    {
+        send_error_response(ERR_MALFORMED_PACKET, from);
         return;
     }
 
-    // validate checksum
-    uint8_t expectedChksum = PKG_START_BYTE + cmd + pin + val + PKG_CHKSUM_PLACEHOLDER_VALUE + PKG_END_BYTE;
-    if (chksum != expectedChksum)
+    uint8_t type = pkg_buffer[0];
+
+    // handle packet types
+    switch (type)
     {
-        // invalid checksum, ignore it
-        sendResponse(cmd, ERR_INVALID_CHECKSUM, false);
+    case TYPE_READ_REQUEST:
+    {
+        // ensure packet length is correct
+        if (pkg_len != 3)
+            break;
+        uint8_t pin = pkg_buffer[1];
+        read_requests_flags flags = {pkg_buffer[2]};
+
+        // ensure pin is valid
+        if (!flags.analog && !IS_VALID_DIGITAL_PIN(pin))
+            break;
+        if (flags.analog && !IS_VALID_ANALOG_PIN(pin))
+            break;
+
+        // convert pin number to pin
+        pin = flags.analog ? PIN_NO_TO_ANALOG_PIN(pin) : PIN_NO_TO_DIGITAL_PIN(pin);
+
+        // set pin mode
+        if (flags.pullup)
+        {
+            pinMode(pin, INPUT_PULLUP);
+        }
+        else if (flags.pulldown)
+        {
+#ifdef INPUT_PULLDOWN
+            pinMode(pin, INPUT_PULLDOWN);
+#else
+            pinMode(pin, INPUT);
+#endif
+        }
+        else
+        {
+            pinMode(pin, INPUT);
+        }
+
+        // read pin value
+        uint16_t value = 0;
+        if (flags.analog)
+        {
+            value = analogRead(pin);
+        }
+        else
+        {
+            value = digitalRead(pin);
+        }
+
+        // send response
+        uint8_t response[4] = {TYPE_READ_RESPONSE, pin, (uint8_t)(value >> 8), (uint8_t)value};
+        sdsp_write_packet(response, 4, OWN_DEVICE_ID, from);
         return;
     }
+    case TYPE_WRITE_REQUEST:
+    {
+        // ensure packet length is correct
+        if (pkg_len != 5)
+            break;
+        uint8_t pin = pkg_buffer[1];
+        uint16_t value = (pkg_buffer[2] << 8) | pkg_buffer[3];
+        write_requests_flags flags = {pkg_buffer[4]};
 
-    // execute command
-    executeCommand(cmd, pin, val);
+        // ensure pin is valid
+        if (!flags.analog && !IS_VALID_DIGITAL_PIN(pin))
+            break;
+        if (flags.analog && !IS_VALID_ANALOG_PIN(pin))
+            break;
+
+        // convert pin number to pin
+        pin = flags.analog ? PIN_NO_TO_ANALOG_PIN(pin) : PIN_NO_TO_DIGITAL_PIN(pin);
+
+        // set pin mode
+        pinMode(pin, OUTPUT);
+
+        // write pin value
+        if (flags.analog)
+        {
+            analogWrite(pin, value);
+        }
+        else
+        {
+            digitalWrite(pin, value);
+        }
+
+        // send response
+        uint8_t response[1] = {TYPE_WRITE_RESPONSE};
+        sdsp_write_packet(response, 1, OWN_DEVICE_ID, from);
+        return;
+    }
+    default:
+        break;
+    }
+
+    // default to error
+    send_error_response(ERR_INVALID_TYPE, from);
 }
 
 void setup()
 {
-    // initialize serial
     Serial.begin(COMM_BAUD_RATE);
 }
 
 void loop()
 {
-    if (Serial.available() > 0)
+    // accept sdsp package
+    uint16_t pkg_len = 0;
+    uint8_t sender_id = 0;
+    read_result_t result = sdsp_read_packet(pkg_buffer, PKG_BUFFER_LEN, &pkg_len, &sender_id, OWN_DEVICE_ID);
+
+    switch (result)
     {
-        acceptPackage();
+    case OK:
+    {
+        // handle package
+        handle_packet(pkg_buffer, pkg_len, sender_id);
+        break;
+    }
+    case NO_END:
+    case CHECKSUM_MISMATCH:
+    case DATA_TOO_LONG:
+        // send error response if package is malformed
+        send_error_response(ERR_MALFORMED_PACKET, sender_id);
+        break;
+    case NO_START:
+    case RECIPIENT_MISMATCH:
+    default:
+        break;
     }
 }
---
+++ arduino/sdsp.h
@@ -0,0 +1,173 @@
+
+//
+// HAL
+//
+// single byte serial read
+uint8_t sdsp_serial_read_blocking();
+
+// single byte serial write
+void sdsp_serial_write(uint8_t data);
+
+//
+// Protocol Constants
+//
+#define SDSP_PKG_START_BYTE 0x7B
+#define SDSP_PKG_END_BYTE 0x7D
+
+typedef enum ReadResult
+{
+    OK,
+    NO_START,
+    NO_END,
+    CHECKSUM_MISMATCH,
+    DATA_TOO_LONG,
+    RECIPIENT_MISMATCH,
+} read_result_t;
+
+//
+// SDSP Implementation
+//
+uint16_t sdsp_crc16(uint8_t *pkg, uint16_t len)
+{
+    uint16_t crc = 0x0000;
+    for (uint16_t i = 0; i < len; i++)
+    {
+        crc ^= pkg[i];
+        for (uint8_t j = 0; j < 8; j++)
+        {
+            if (crc & 0x0001)
+            {
+                crc >>= 1;
+                crc ^= 0x1021;
+            }
+            else
+            {
+                crc >>= 1;
+            }
+        }
+    }
+
+    return crc;
+}
+
+void sdsp_ffwd_to_end(uint16_t len)
+{
+    // read at least len bytes
+    for (uint16_t i = 0; i < len; i++)
+    {
+        sdsp_serial_read_blocking();
+    }
+
+    // fast-forward to the next packet end marker
+    while (sdsp_serial_read_blocking() != SDSP_PKG_END_BYTE)
+        ;
+}
+
+read_result_t sdsp_read_packet(uint8_t *buffer,
+                               uint16_t buffer_len,
+                               uint16_t &pkg_len,
+                               uint8_t &pkg_sender_id,
+                               uint8_t own_id)
+{
+    // check for the start of packet
+    uint16_t i = 0;
+    if ((buffer[i++] = sdsp_serial_read_blocking()) != SDSP_PKG_START_BYTE)
+        return NO_START;
+
+    // read sender and receiver IDs
+    pkg_sender_id = sdsp_serial_read_blocking();
+    uint8_t pkg_receiver_id = sdsp_serial_read_blocking();
+    buffer[i++] = pkg_sender_id;
+    buffer[i++] = pkg_receiver_id;
+
+    // read packet data length
+    buffer[i++] = sdsp_serial_read_blocking(); // MSB
+    buffer[i++] = sdsp_serial_read_blocking(); // LSB
+    pkg_len = (buffer[i - 2] << 8) | buffer[i - 1];
+
+    // ensure the packet will fit into the buffer
+    uint16_t buffer_used_len = (pkg_len + 8);
+    if (buffer_used_len > buffer_len)
+    {
+        // will not fit, fast-forward to the end of the packet
+        sdsp_ffwd_to_end(pkg_len);
+        return DATA_TOO_LONG;
+    }
+
+    // ensure the packet is for us
+    if (pkg_receiver_id != own_id && pkg_receiver_id != 0xFF)
+    {
+        // not for us, fast-forward to the end of the packet
+        sdsp_ffwd_to_end(pkg_len);
+        return RECIPIENT_MISMATCH;
+    }
+
+    // read the packet data
+    for (uint16_t j = 0; j < pkg_len; j++)
+    {
+        buffer[i++] = sdsp_serial_read_blocking();
+    }
+
+    // read the checksum
+    buffer[i++] = sdsp_serial_read_blocking(); // MSB
+    buffer[i++] = sdsp_serial_read_blocking(); // LSB
+    uint16_t chksum = (buffer[i - 2] << 8) | buffer[i - 1];
+
+    // read the end of packet
+    if ((buffer[i++] = sdsp_serial_read_blocking()) != SDSP_PKG_END_BYTE)
+        return NO_END;
+
+    // validate the checksum
+    uint16_t expected_chksum = sdsp_crc16(buffer, buffer_used_len);
+    if (chksum != expected_chksum)
+        return CHECKSUM_MISMATCH;
+
+    // all OK, remove the prologue from the buffer
+    for (int i = 0; i < buffer_used_len; i++)
+    {
+        buffer[i] = buffer[i + 5];
+    }
+
+    return OK;
+}
+
+void sdsp_write_packet(uint8_t *body,
+                       uint16_t body_len,
+                       uint8_t sender_id,
+                       uint8_t receiver_id)
+{
+    // allocate a buffer for the packet
+    uint16_t pkg_len = body_len + 5;
+    uint8_t pkg[pkg_len];
+
+    // assemble packet:
+    uint16_t i = 0;
+    // prolouge
+    pkg[i++] = SDSP_PKG_START_BYTE;
+    pkg[i++] = sender_id;
+    pkg[i++] = receiver_id;
+    pkg[i++] = (body_len >> 8) & 0xFF;
+    pkg[i++] = body_len & 0xFF;
+
+    // body
+    for (uint16_t j = 0; j < body_len; j++)
+    {
+        pkg[i++] = body[j];
+    }
+
+    // epilogue
+    pkg[i++] = 0x00; // checksum MSB
+    pkg[i++] = 0x00; // checksum LSB
+    pkg[i++] = SDSP_PKG_END_BYTE;
+
+    // calculate and write the checksum
+    uint16_t chksum = sdsp_crc16(pkg, pkg_len);
+    pkg[i - 3] = (chksum >> 8) & 0xFF;
+    pkg[i - 2] = chksum & 0xFF;
+
+    // write the packet
+    for (uint16_t j = 0; j < pkg_len; j++)
+    {
+        sdsp_serial_write(pkg[j]);
+    }
+}
